2c21a9149489dc80c356619c4bfcd89b45d9eaec
Wilhelm Liao <williaml.twcp@gmail.com>
2021-01-23 14:38:39 +0800

    fix: typo

--- internal/test/eventLogType_test.go
+++ internal/test/eventLogType_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestEventLogType(t *testing.T) {
 	if log.NONE.IsValid() {
-		t.Errorf("assert 'EventLogType.NONE.IsValid()':: excepted '%v', got '%v'", true, log.NONE.IsValid())
+		t.Errorf("assert 'EventLogType.NONE.IsValid()':: expected '%v', got '%v'", true, log.NONE.IsValid())
 	}
 	if !log.PASS.IsValid() {
-		t.Errorf("assert 'EventLogType.PASS.IsValid()':: excepted '%v', got '%v'", false, log.PASS.IsValid())
+		t.Errorf("assert 'EventLogType.PASS.IsValid()':: expected '%v', got '%v'", false, log.PASS.IsValid())
 	}
 	if !log.FAIL.IsValid() {
-		t.Errorf("assert 'EventLogType.FAIL.IsValid()':: excepted '%v', got '%v'", false, log.FAIL.IsValid())
+		t.Errorf("assert 'EventLogType.FAIL.IsValid()':: expected '%v', got '%v'", false, log.FAIL.IsValid())
 	}
 }
--- internal/test/eventLog_test.go
+++ internal/test/eventLog_test.go
@@ -46,30 +46,30 @@ func TestWriteMarshal(t *testing.T) {
 		fmt.Printf("> model\n< %#v\n", model)
 
 		if model["type"] != logType.Name() {
-			t.Errorf("assert 'type':: excepted '%v', got '%v'", logType.Name(), model["type"])
+			t.Errorf("assert 'type':: expected '%v', got '%v'", logType.Name(), model["type"])
 		}
-		exceptedSeverity := strconv.FormatInt(int64(logType.Severity()), 10)
-		if strconv.FormatFloat(model["severity"].(float64), 'G', 20, 64) != exceptedSeverity {
-			t.Errorf("assert 'severity':: excepted '%v', got '%v'", exceptedSeverity, model["severity"])
+		expectedSeverity := strconv.FormatInt(int64(logType.Severity()), 10)
+		if strconv.FormatFloat(model["severity"].(float64), 'G', 20, 64) != expectedSeverity {
+			t.Errorf("assert 'severity':: expected '%v', got '%v'", expectedSeverity, model["severity"])
 		}
 		if model["event_id"] != eventID {
-			t.Errorf("assert 'event_id':: excepted '%v', got '%v'", eventID, model["event_id"])
+			t.Errorf("assert 'event_id':: expected '%v', got '%v'", eventID, model["event_id"])
 		}
 		if model["category"] != category {
-			t.Errorf("assert 'categoty':: excepted '%v', got '%v'", category, model["category"])
+			t.Errorf("assert 'categoty':: expected '%v', got '%v'", category, model["category"])
 		}
 		if model["source"] != source {
-			t.Errorf("assert 'source':: excepted '%v', got '%v'", source, model["source"])
+			t.Errorf("assert 'source':: expected '%v', got '%v'", source, model["source"])
 		}
 		if model["version"] != version {
-			t.Errorf("assert 'version':: excepted '%v', got '%v'", version, model["version"])
+			t.Errorf("assert 'version':: expected '%v', got '%v'", version, model["version"])
 		}
 		if model["message"] != message {
-			t.Errorf("assert 'message':: excepted '%v', got '%v'", message, model["message"])
+			t.Errorf("assert 'message':: expected '%v', got '%v'", message, model["message"])
 		}
-		exceptedTimestamp := strconv.FormatInt(timestamp.UnixNano()/int64(time.Millisecond), 10)
-		if strconv.FormatFloat(model["timestamp"].(float64), 'G', 20, 64) != exceptedTimestamp {
-			t.Errorf("assert 'timestamp':: excepted '%v', got '%v'", exceptedTimestamp, model["timestamp"])
+		expectedTimestamp := strconv.FormatInt(timestamp.UnixNano()/int64(time.Millisecond), 10)
+		if strconv.FormatFloat(model["timestamp"].(float64), 'G', 20, 64) != expectedTimestamp {
+			t.Errorf("assert 'timestamp':: expected '%v', got '%v'", expectedTimestamp, model["timestamp"])
 		}
 	}
 
@@ -82,26 +82,26 @@ func TestWriteMarshal(t *testing.T) {
 		fmt.Printf("> EventLog\n< %#v\n", event)
 
 		if event.Type != logType {
-			t.Errorf("assert 'EventLog.Type':: excepted '%v', got '%v'", logType, event.Type)
+			t.Errorf("assert 'EventLog.Type':: expected '%v', got '%v'", logType, event.Type)
 		}
 		if event.EventID != eventID {
-			t.Errorf("assert 'EventLog.EventID':: excepted '%v', got '%v'", eventID, event.EventID)
+			t.Errorf("assert 'EventLog.EventID':: expected '%v', got '%v'", eventID, event.EventID)
 		}
 		if event.Category != category {
-			t.Errorf("assert 'EventLog.Category':: excepted '%v', got '%v'", category, event.Category)
+			t.Errorf("assert 'EventLog.Category':: expected '%v', got '%v'", category, event.Category)
 		}
 		if event.Source != source {
-			t.Errorf("assert 'EventLog.Source':: excepted '%v', got '%v'", source, event.Source)
+			t.Errorf("assert 'EventLog.Source':: expected '%v', got '%v'", source, event.Source)
 		}
 		if event.Version != version {
-			t.Errorf("assert 'EventLog.Version':: excepted '%v', got '%v'", version, event.Version)
+			t.Errorf("assert 'EventLog.Version':: expected '%v', got '%v'", version, event.Version)
 		}
 		if event.Message != message {
-			t.Errorf("assert 'EventLog.Message':: excepted '%v', got '%v'", message, event.Message)
+			t.Errorf("assert 'EventLog.Message':: expected '%v', got '%v'", message, event.Message)
 		}
-		exceptedTimestamp := timestamp.UnixNano() / int64(time.Millisecond)
-		if event.Timestamp != exceptedTimestamp {
-			t.Errorf("assert 'EventLog.Timestamp':: excepted '%v', got '%v'", exceptedTimestamp, event.Timestamp)
+		expectedTimestamp := timestamp.UnixNano() / int64(time.Millisecond)
+		if event.Timestamp != expectedTimestamp {
+			t.Errorf("assert 'EventLog.Timestamp':: expected '%v', got '%v'", expectedTimestamp, event.Timestamp)
 		}
 	}
 
